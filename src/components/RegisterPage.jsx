11c467e6bc40a66ce01498cc39d873fecc9894d0
miguelperson <mi529437@ucf.edu>
2024-06-20 11:19:18 -0400

    was finally able to implement some functionality in register function, want to be able to set proper registration parameters

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -3,15 +3,23 @@ import { useNavigate } from 'react-router-dom';
 function registerPage() {
     // let name = "miguel";
     const [input1, setInput1] = useState('');
+    const registerButton = () => {
+        alert(input1)
+    }
+    const enterName = event => {
+        setInput1(event.target.value)
+    }
 
     return (
         <div>
             <h1>Registration Page</h1>
             <p>this is a test... the bird is the word</p>
             <input
+                onChange={enterName }
                 className="component-input"
                 type="text"
                 placeholder="Full Name"
+                value={input1 }
             />
 
             <input
@@ -36,6 +44,7 @@ function registerPage() {
             />
 
             <input type="submit" value="Create Account" className="component-button"></input>
+            <button type="submit" value="CreateAccoutnt" className="component-button" onClick={registerButton}>HABIBI</button>
         </div>
         
         
