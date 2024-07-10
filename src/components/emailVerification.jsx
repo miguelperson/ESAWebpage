9e90ccf3c00cde23d6faac304785d13a939ae3ca
miguelperson <mi529437@ucf.edu>
2024-07-10 11:40:33 -0400

    misplaced vairables

--- src/components/emailVerification.jsx
+++ src/components/emailVerification.jsx
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
+
 function EmailVerification() {
+    const [verificationCode, setCode] = useState('');
 
     return (
         <div>
             <h1>ESA Parcel Search</h1>
+            <input
+                onChange={e => setCode(e.target.value)}
+                className="component-input"
+                type="email"
+                placeholder="Confirmation Code"
+                value={verificationCode}
+            />
         </div>
 
     );
